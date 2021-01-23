d87b7dacfe7948bcd2b033485dd1cf0572777c20
Amit Mungare <amungare27@gmail.com>
2021-01-23 15:27:10 +0530

    Designing the layout

---
+++ app/src/main/java/com/example/tictactoe/winner.kt
@@ -0,0 +1,13 @@
+package com.example.tictactoe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_winner.*
+
+class winner : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_winner)
+
+    }
+}
